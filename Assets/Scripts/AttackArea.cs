c0ba10da916ef0cf498f7357c6d0df73700984f3
scv829 <scv829@naver.com>
2024-09-27 14:53:04 +0900

    Feat: 몬스터 공격 고도화
    몬스터 공격 고도화
    
    Related to: #3

---
+++ Assets/Scripts/AttackArea.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackArea : MonoBehaviour
+{
+    [Header("Attack")]
+    [SerializeField] Transform target;
+
+    public Transform Target { get { return target; } }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if ((other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Nexus")) && target == null)
+        {
+            target = other.transform;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if ((other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Nexus")) && target == null)
+        {
+            target = other.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (target != null && other.gameObject.Equals(target.gameObject))
+        {
+            target = null;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, transform.GetComponent<SphereCollider>().radius);
+    }
+
+}
